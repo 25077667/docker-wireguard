0fd543d214652101b393a057183e204069794c1f
25077667 <zxc25077667@protonmail.com>
2021-06-29 10:45:04 +0800

    Add get_sys_info supply

--- vpn-agent/go.mod
+++ vpn-agent/go.mod
@@ -3,9 +3,14 @@ module monitor
 go 1.16
 
 require (
+	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
 	github.com/andybalholm/brotli v1.0.3 // indirect
-	github.com/gofiber/fiber/v2 v2.13.0 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/gofiber/fiber/v2 v2.13.0
 	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/shirou/gopsutil v3.21.5+incompatible
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.6 // indirect
 	github.com/valyala/fasthttp v1.27.0 // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 )
--- vpn-agent/monitor.go
+++ vpn-agent/monitor.go
@@ -1,23 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 )
 
-func get_sys_info() string {
-	// TODO: Get the runtime metrics
-	return ""
+var cpu_info_lock sync.RWMutex
+
+var cpu_rate float64
+
+func get_cpu() {
+	for {
+		me, _ := cpu.Percent(time.Second, true)
+		rate := float64(0)
+		for i := 0; i < len(me); i++ {
+			rate += me[i] / float64(len(me))
+		}
+
+		cpu_info_lock.Lock()
+		cpu_rate = rate
+		cpu_info_lock.Unlock()
+	}
+}
+
+func get_sys_info() []byte {
+	type Percent struct {
+		Mem float64 `json:Mem`
+		CPU float64 `json:CPU`
+	}
+
+	res := &Percent{
+		Mem: func() float64 {
+			v, _ := mem.VirtualMemory()
+			return v.UsedPercent
+		}(),
+		CPU: func() float64 {
+			cpu_info_lock.RLock()
+			me := cpu_rate
+			cpu_info_lock.RUnlock()
+			return me
+		}(),
+	}
+
+	s, _ := json.Marshal(res)
+	return s
 }
 
 func main() {
+	go get_cpu()
 	app := fiber.New()
 	app.Get("/stat", func(c *fiber.Ctx) error {
-		return c.SendString(get_sys_info())
+		return c.Send(get_sys_info())
 	})
-	app.Get("/key", func(c *fiber.Ctx) error {
-		id := c.Get("id", "") // Suppose it is a string of int
+	app.Get("/peer:id?", func(c *fiber.Ctx) error {
+		id := c.Params("id") // Suppose it is a string of int
 		path := fmt.Sprintf("/config/peer%s/peer%s.conf", id, id)
 		return c.SendFile(path) // give Sendfile to check if the file is exist
 	})
